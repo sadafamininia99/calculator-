1c70b74071c01b379c3cf9bf683f467cfcec7be8
SadafAmininiiiaa <aminicomputer96@gmail.com>
2022-11-18 21:34:47 +0330

    html part

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
-calculator 
+import React, { useState } from 'react'
+
+function App() {
+    const [result, SetResult] = useState('')
+    return (
+        <div className="container">
+            <form>
+                <input type={'text'} value={result} />
+
+
+            </form>
+            <div className='buttons'>
+ 
+            </div>
+
+        </div>
+    )
+}
+
+export default App
